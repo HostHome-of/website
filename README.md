74e826f0849a1966de503367fbe207961351e30c
maubg-debug <maubg2007@gmail.com>
2021-01-14 20:39:22 +0100

    Un CLI menos

--- README.md
+++ README.md
@@ -15,3 +15,9 @@
     * Clojure - Ayuda
 * Hacer el systema del hosting
 * Hacer que cadavez que vea el codigo no vomite
+* Termninar la pagina de la cuenta y responsive
+
+    ![img](https://cdn.dribbble.com/users/1553101/screenshots/14115417/media/02384a59f76d41de86fd30425ef49ba1.png?compress=1&resize=1000x750)
+
+* Empezar algun CLI
+* Continuar con los docs
---
+++ src/cli/ruby/main.rb
@@ -0,0 +1,67 @@
+require 'optparse'
+require "uri"
+
+@verbose = false
+
+@ayuda = File.open("./src/cli/ayuda.txt", "r").read
+@texto = File.open("./src/cli/archivo.txt", "r").read
+
+@repo = ""
+@main = ""
+
+OptionParser.new do |opts|
+  opts.on("-h", "--help", "Enseña el comando de ayuda") do
+    puts @ayuda
+    exit
+  end
+  opts.on("-v", "--verbose", "te dice lo que esta creando") do
+    @verbose = true
+  end
+  opts.on("empezar", "Empezar la instalacion") do
+
+    while true # Archivo principal
+      puts "Pon donde estara el archivo principal * :: (Tiene que terminar con .rb)"
+      main = gets
+      if main == ""
+        next
+      elsif !main.end_with?(".rb")
+      end
+      @main = main
+      break
+    end
+
+    while true # Repo (Github, Bitbucket)
+      puts "Pon la url del repositorio (GitHub o BitBucket) * :: "
+      repo = gets
+      if repo == ""
+        next
+      end
+      urls = URI.extract(repo)
+      if urls.length == 1
+        if repo.start_with?("https://github.com/") or repo.start_with?("https://bitbucket.org/")
+          @repo = repo
+          break
+        end
+      end
+    end
+
+    @texto.gsub! 'mainTemp', @main
+    @texto.gsub! 'repoTemp', @repo
+
+    if @verbose == true
+      puts "Creando archivo en ruta padre ..."
+      File.open(".host.home", "w") { 
+        |file| file.puts @texto
+      }
+      sleep(1)
+      puts "Escribiendo ..."
+      sleep(1)
+      puts "Ya esta"
+      exit
+    else
+      File.open(".host.home", "w") { 
+        |file| file.puts @texto
+      }
+    end
+  end
+end.parse!
