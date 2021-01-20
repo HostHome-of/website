bd36fa08e12f101b5bc2a447e0eba22cbb824f9b
maubg-debug <maubg2007@gmail.com>
2021-01-20 20:50:53 +0100

    d

--- src/static/js/auth.js
+++ src/static/js/auth.js
@@ -1,20 +1,38 @@
-const inputs = document.querySelectorAll(".input");
+var inputField = document.querySelectorAll("input"),
+    inputWrapper = document.querySelectorAll(".input-wrapper"),
+    modal = document.querySelector(".modal"),
+    btnClose = document.querySelector(".btn-close"),
+    btnSign = document.querySelector(".sign"),
+    daily = document.querySelector(".daily");
 
+btnSign.addEventListener("click", openModal);
+btnClose.addEventListener("click", closeModal);
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
-}
-
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
-}
-
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+inputField.forEach(function(el) {
+   el.addEventListener("focus", animeInput);
+   el.addEventListener("focusout", removeAnimeInput);
 });
+
+function openModal() {
+   modal.classList.add("open");
+}
+
+function closeModal() {
+   modal.classList.add("close");
+
+   setTimeout(function() {
+      
+      modal.classList.remove("open");
+      modal.classList.remove("close");
+   }, 1500);
+}
+
+
+function animeInput(event) {
+   event.target.closest(".input-wrapper").classList.add("active");
+}
+function removeAnimeInput(event) {
+   if (event.target.value == "") {
+      event.target.closest(".input-wrapper").classList.remove("active");
+   }
+}
