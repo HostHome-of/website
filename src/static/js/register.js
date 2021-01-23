819d80059d0158bcff59ce21bb84e844c9164088
maubg-debug <maubg2007@gmail.com>
2021-01-23 16:58:35 +0100

    Mejoras

--- src/static/js/register.js
+++ src/static/js/register.js
@@ -1,7 +1,18 @@
 async function enviar(e) {
     Notiflix.Loading.Init({svgColor:"#6493c6",});
     e.preventDefault();
+
     let no_es_valido = false;
+    let no_es_valido_psw = false;
+
+
+    if (document.getElementById("password").value.length < 6) {
+        Notiflix.Notify.Failure('La contraseña debe de ser mas de 6 caracteres.');
+        no_es_valido_psw = true
+        setTimeout(function() {
+            return false;
+        }, 5000);
+    }
     await fetch("/register?nm="+ document.getElementById("name").value + "&psw=" + document.getElementById("password").value + "&mail=" + document.getElementById("email").value, {
         method: 'POST'
     }).then(response => response.json()).then(function (data) {
@@ -10,17 +21,19 @@ async function enviar(e) {
         }
     });
 
-    if (no_es_valido == true) {
+    if (no_es_valido == true && no_es_valido_psw == false) {
         Notiflix.Notify.Failure('Ese email ya existe');
         setTimeout(function() {
             return false;
         }, 5000);
     } else {
-        Notiflix.Loading.Circle();
-        setTimeout(function() {
-            window.location.replace("/account");
-            return false;
-        }, 3000);
+        if (no_es_valido_psw == false) {
+            Notiflix.Loading.Circle();
+            setTimeout(function() {
+                window.location.replace("/account");
+                return false;
+            }, 3000);
+        }
     }
     return false;
 }
